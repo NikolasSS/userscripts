d90853b2b61ab18a5b8db6f4d232ac1026d3b746
Nik <Dev.NikolasSS@gmail.com>
2016-04-07 21:04:41 -0500

    debugging lines removed

--- r-all-links-filtered/r-all-links-filtered.user.js
+++ r-all-links-filtered/r-all-links-filtered.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         r-all-links-filtered
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  All /r/All links point to filtered /r/All
-// @author       /u/SpaceNacho
+// @author       /u/SpaceNacho (NikolasSS)
 // @match        https://www.reddit.com/*
 // @grant        none
 // ==/UserScript==
@@ -13,11 +13,8 @@
 
     var links = document.getElementsByTagName("a");
     for (var i = 0, l = links.length; i < l; i++) {
-        // the only /r/All link apperas to be (i == 104)
-        // leaving loop in case that changes
         var link = links[i];
         if (link.href === 'https://www.reddit.com/r/all') {
-            console.log(i);
             link.innerHTML = "All (Filtered)";
             link.href = "https://www.reddit.com/me/f/all/";
         }
